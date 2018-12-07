c04bde19c586e2830803a96ba089cb9292c16aaa
hiro15 <tkgm15h@gmail.com>
2018-12-08 04:54:35 +0900

    Implement settings APIs

--- Settings.go
+++ Settings.go
@@ -1,5 +1,58 @@
 package gonetworkmanager
 
-const (
-	SettingsInterface = NetworkManagerInterface + ".Settings"
+import (
+	"github.com/godbus/dbus"
 )
+
+const (
+	SettingsInterface  = NetworkManagerInterface + ".Settings"
+	SettingsObjectPath = NetworkManagerObjectPath + "/Settings"
+
+	SettingsListConnections = SettingsInterface + ".ListConnections"
+	SettingsAddConnection   = SettingsInterface + ".AddConnection"
+)
+
+type Settings interface {
+
+	// ListConnections gets list the saved network connections known to NetworkManager
+	ListConnections() []Connection
+
+	// AddConnection call new connection and save it to disk.
+	AddConnection(settings ConnectionSettings) Connection
+}
+
+func NewSettings() (Settings, error) {
+	var s settings
+	return &s, s.init(NetworkManagerInterface, SettingsObjectPath)
+}
+
+type settings struct {
+	dbusBase
+}
+
+func (s *settings) ListConnections() []Connection {
+	var connectionPaths []dbus.ObjectPath
+
+	s.call(&connectionPaths, SettingsListConnections)
+	connections := make([]Connection, len(connectionPaths))
+
+	var err error
+	for i, path := range connectionPaths {
+		connections[i], err = NewConnection(path)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	return connections
+}
+
+func (s *settings) AddConnection(settings ConnectionSettings) Connection {
+	var path dbus.ObjectPath
+	s.call(&path, SettingsAddConnection, settings)
+	con, err := NewConnection(path)
+	if err != nil {
+		panic(err)
+	}
+	return con
+}
